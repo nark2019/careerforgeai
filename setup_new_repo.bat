96b5ce48d74ebac3fcc67444a82738b28da64a27
narkisho <narkisho@gmail.com>
2025-06-19 11:41:49 +0300

    Update client submodule with all files

---
+++ setup_new_repo.bat
@@ -0,0 +1,11 @@
+@echo off
+echo Setting up repository for nark2019 account...
+git remote set-url origin https://nark2019@github.com/nark2019/careerforgeai.git
+echo Remote URL updated.
+echo.
+echo Now you need to:
+echo 1. Go to https://github.com/nark2019
+echo 2. Create a new repository named "careerforgeai"
+echo 3. Then run: git push -u origin master
+echo.
+pause 
